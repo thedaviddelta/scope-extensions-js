1bc7b04fe785acb4082b860ab3f504968d6ef932
David <thedaviddelta@gmail.com>
2020-04-26 20:23:14 +0200

    TSDoc + Travis

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # scope-extensions-js
+
+[![Build Status](https://travis-ci.com/TheDavidDelta/scope-extensions-js.svg?branch=master)](https://travis-ci.com/TheDavidDelta/scope-extensions-js)
+[![License](https://img.shields.io/github/license/TheDavidDelta/scope-extensions-js)](./LICENSE)
+
+<img src="img/logo.png" width="128" align="right">
+
+Package for using [Kotlin's Scope Function Extensions](https://kotlinlang.org/docs/reference/scope-functions.html) on JavaScript and TypeScript.
+
+It also supports the use of the new [Optional Chaining Operator](https://github.com/tc39/proposal-optional-chaining), bringing the logic of [Kotlin's Null Safe Calls](https://kotlinlang.org/docs/reference/null-safety.html) to JavaScript.
+
+---
+
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,29 @@ export {};
 
 declare global {
     interface Object {
+        /**
+         * Calls the specified function block with `this` value as its argument and returns its result
+         * @param block - The function to be executed with `this` as argument
+         * @returns `block`'s result
+         */
         let<T, R>(this: T | null | undefined, block: (it: T) => R): R;
+        /**
+         * Calls the specified function block with `this` value as its argument and returns `this` value
+         * @param block - The function to be executed with `this` as argument
+         * @returns `this`
+         */
         also<T>(this: T | null | undefined, block: (it: T) => void): T;
+        /**
+         * Calls the specified function block with `this` value as its receiver and returns its value
+         * @param block - The function to be executed with `this` as context
+         * @returns `block`'s result
+         */
         run<T, R>(this: T | null | undefined, block: (this: T) => R): R;
+        /**
+         * Calls the specified function block with `this` value as its receiver and returns `this` value
+         * @param block - The function to be executed with `this` as context
+         * @returns `this`
+         */
         apply<T>(this: T | null | undefined, block: (this: T) => void): T;
     }
 }
